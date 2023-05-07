45216d59be30e2516ad47118e67dbd69596669eb
Sergei Cheremnov <s.cheremnov@innopolis.ru>
2023-05-07 08:37:43 +0300

    init nuxt project (wip)

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/ui'
+  ],
+  // app: {
+  //   head: {
+  //     script: {
+        
+  //     }
+  //   }
+  // }
+})
